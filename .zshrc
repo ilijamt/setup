4e4dc8e184dce39935d2c3e81d1bc8556ebaff68
Ilija Matoski <ilijamt@gmail.com>
2019-06-13 19:22:24 +0200

    Reorganize a bit to make it easier to load different shells and configuration

--- .zshrc
+++ .zshrc
@@ -1,3 +1,7 @@
 for f in $HOME/.conf.d/*.conf; do
    source $f
 done
+
+for f in $HOME/.conf.d/zsh/*.conf; do
+   source $f
+done
---
+++ setup.conf.d.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Copy the configuration files to the your home directory at $HOME"
+cp -Rv ./.zshrc ./.conf.d/ $HOME
