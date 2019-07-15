a65acd76ec7ec50d80e0910637ec7fbd23851853
Ilija Matoski <ilijamt@gmail.com>
2019-07-15 14:02:06 +0200

    Correct versions of plugins available now

--- scripts/powerlevel.sh
+++ scripts/powerlevel.sh
@@ -12,9 +12,9 @@ function git_pull_or_clone() {
 }
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git_pull_or_clone $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-completions https://github.com/zsh-users/zsh-syntax-completions
-git_pull_or_clone $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-autosuggestions https://github.com/zsh-users/zsh-syntax-autosuggestions
-git_pull_or_clone $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting
+git_pull_or_clone $HOME/.oh-my-zsh/custom/plugins/zsh-completions https://github.com/zsh-users/zsh-completions.git
+git_pull_or_clone $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions.git
+git_pull_or_clone $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting.git
 git_pull_or_clone $HOME/.oh-my-zsh/custom/plugins/history-search-multi-word https://github.com/zdharma/history-search-multi-word.git
 git_pull_or_clone $HOME/.oh-my-zsh/themes/powerlevel9k https://github.com/bhilburn/powerlevel9k.git
 git_pull_or_clone $HOME/.oh-my-zsh/themes/powerlevel10k https://github.com/romkatv/powerlevel10k.git
